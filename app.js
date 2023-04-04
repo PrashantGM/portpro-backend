737d0e483881e83f82d7ada472308aab11421180
Prashant Gharti Magar <prashant.ghartimagar@tai.com.np>
2023-04-04 16:52:48 +0545

    feat: inital express setup

---
+++ app.js
@@ -0,0 +1,12 @@
+import * as express from 'express';
+import { config } from 'dotenv';
+config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
