65cef599ed1118abbf8b1371f2b48f759b970282
Prashant Gharti Magar <prashant.ghartimagar@tai.com.np>
2023-04-06 13:48:17 +0545

    feat: implement helmet and express-rate-limit

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
 require('dotenv').config();
-
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const { connectDB } = require('./databases/mongo');
 const router = require('./routes/user.route');
 const notFound = require('./middlewares/notFound');
 const { connectMySQL } = require('./databases/mysql');
 
+const PORT = process.env.PORT;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 200,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 const app = express();
+
+app.use(helmet);
+app.use(limiter);
 
 app.use(router);
 app.use(notFound);
-const PORT = process.env.PORT;
 
 const start = async () => {
   try {
